d928f020c878b1cdebf9ef576098b06ca318da48
Olaoye <oyekunlejohnson8@gmail.com>
2022-12-07 01:58:12 +0100

    addede eureka client to the services

--- inventory-service/src/main/java/com/olaoye/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/olaoye/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,13 @@ import com.olaoye.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
